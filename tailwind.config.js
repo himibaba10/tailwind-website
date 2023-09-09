b88f2c6610e1c00775c186f84fc04c2693c4cc3e
himibaba10 <himibaba10@gmail.com>
2023-09-10 03:54:18 +0600

    Finished the whole project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#363958",
+        secondary: "#FABE4C",
+        para: "#3E3E3E",
+      },
+    },
+  },
+  plugins: [],
+};
